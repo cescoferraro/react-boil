74ce8a2d91ca9fba22973896ec774c075bcb009a
cescoferraro <francescoaferraro@gmail.com>
2017-07-07 12:17:51 -0300

    latest

--- server/server.js
+++ server/server.js
@@ -8,10 +8,11 @@ app.use('/js', express.static(path.join(__dirname, 'js')));
 app.use('/vendor', express.static(path.join(__dirname, 'vendor')));
 app.use('/icons', express.static(path.join(__dirname, 'icons')));
 app.use('/css', express.static(path.join(__dirname, 'css')));
+app.use('/appcache', express.static(path.join(__dirname, 'appcache')));
 const clientStats = require('./stats.json')
 const outputPath = __dirname 
-app.get('/service-worker.js', (req, res) => {
-    res.sendFile("./service-worker.js",{root: "./"});
+app.get('/sw.js', (req, res) => {
+    res.sendFile("./sw.js",{root: "./"});
 });
 app.use(require("./server/main").default({ production: true, clientStats , outputPath}));
 app.listen(5000);
--- shared/helmet.tsx
+++ shared/helmet.tsx
@@ -7,7 +7,8 @@ export const MyHelmet = ({ title }) => (
         <html lang="pt" />
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-	<link rel="manifest" href="/icons/manifest.json" />
+        <link rel="manifest" href="/icons/manifest.json" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
         <link rel="shortcut icon" href="icons/favicon.ico" />
         <meta name="theme-color" content="#00bfff" />
         <title>{title + " | React-boil"} </title>
