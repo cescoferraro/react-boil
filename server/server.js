4c99855d0680348c8f79d388c6c73892114a97e5
cescoferraro <francescoaferraro@gmail.com>
2017-07-19 19:04:19 -0300

    add caching

--- server/server.js
+++ server/server.js
@@ -1,26 +1,34 @@
-const express = require("express");
-const morgan = require("morgan");
+const express = require('express');
+const morgan = require('morgan');
 const app = express();
-const path = require("path");
-app.disable("x-powered-by");
-app.use(morgan("combined"));
-app.use("/js", express.static(path.join(__dirname, "js")));
-app.use("/vendor", express.static(path.join(__dirname, "vendor")));
-app.use("/icons", express.static(path.join(__dirname, "icons")));
-app.use("/css", express.static(path.join(__dirname, "css")));
-app.use("/html", express.static(path.join(__dirname, "html")));
-app.use("/images", express.static(path.join(__dirname, "images")));
-app.use("/appcache", express.static(path.join(__dirname, "appcache")));
-const clientStats = require("./stats.json");
+const path = require('path');
+const maxAge = { maxAge: 86400000 };
+const maxAgeII = { root: './', maxAge: 86400000 };
+app.disable('x-powered-by');
+app.use(morgan('combined'));
+app.use('/js', express.static(path.join(__dirname, 'js'), maxAge));
+app.use('/vendor', express.static(path.join(__dirname, 'vendor'), maxAge));
+app.use('/icons', express.static(path.join(__dirname, 'icons'), maxAge));
+app.use('/css', express.static(path.join(__dirname, 'css'), maxAge));
+app.use('/html', express.static(path.join(__dirname, 'html'), maxAge));
+app.use('/images', express.static(path.join(__dirname, 'images'), maxAge));
+app.use('/appcache', express.static(path.join(__dirname, 'appcache'), maxAge));
+const clientStats = require('./stats.json');
 const outputPath = __dirname;
-app.get("/service-worker.js", (req, res) => {
-    res.sendFile("./service-worker.js", {root: "./"});
+app.get('/service-worker.js', (req, res) => {
+  res.sendFile('./service-worker.js', maxAgeII);
 });
-app.get("/sw.js", (req, res) => {
-    res.sendFile("./sw.js", {root: "./"});
+app.get('/sw.js', (req, res) => {
+  res.sendFile('./sw.js', maxAgeII);
 });
-app.get("/index.html", (req, res) => {
-    res.sendFile("./index.html", {root: "./html"});
+app.get('/index.html', (req, res) => {
+  res.sendFile('./index.html', maxAgeII);
 });
-app.use(require("./server/main").default({ production: true, clientStats , outputPath}));
+app.use(
+  require('./server/main').default({
+    production: true,
+    clientStats,
+    outputPath
+  })
+);
 app.listen(5000);
